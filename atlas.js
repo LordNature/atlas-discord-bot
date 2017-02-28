ae88187264817be2065e04c6e43784c001b8f1ee
YOUR NAME <lordnature.christian@gmail.com>
2017-02-27 22:38:32 -0600

    Added more commands

--- atlas.js
+++ atlas.js
@@ -32,10 +32,10 @@ commands.atlas = {
   exec: function(msg, args) {
     var info = new discord.RichEmbed()
     info.setAuthor("Atlas")
-    info.setColor("#00FBFF")
-    info.setDescription("I'm a Discord bot that handles a variety of tasks to make <@122805215043780609>'s life easier!")
-    info.setThumbnail(atlas.user.avatarURL)
-    info.addField("Commands", "For a list of commands please type `" + config.prefix + "help`", true)
+        .setColor("#00FBFF")
+        .setDescription("I'm a Discord bot that handles a variety of tasks to make <@122805215043780609>'s life easier!")
+        .setThumbnail(atlas.user.avatarURL)
+        .addField("Commands", "For a list of commands please type `" + config.prefix + "help`", true)
     msg.channel.sendEmbed(info)
   }
 }
@@ -51,11 +51,27 @@ commands.status = {
         hrs = Math.round(mins / 60),
         info = new discord.RichEmbed()
     info.setAuthor("Atlas")
-    info.setColor("#00FBFF")
-    info.setDescription("Debug Information about Atlas.")
-    info.setThumbnail(atlas.user.avatarURL)
-    info.addField("Uptime", hrs + ":" + mins + ":" + secs, true)
+        .setColor("#00FBFF")
+        .setDescription("Debug Information about Atlas.")
+        .setThumbnail(atlas.user.avatarURL)
+        .addField("Node Version", process.version, true)
+        .addField("Uptime", hrs + ":" + mins + ":" + secs, true)
+        .addField("Dependencies", `**Discord.js:** v${discord.version}`)
+        .addField("RAM", Math.round(process.memoryUsage().rss / 1024 / 1000) + "MB", true)
     msg.channel.sendEmbed(info)
+  }
+}
+
+commands.reboot = {
+  name: "reboot",
+  desc: "Reboots Atlas.",
+  adminOnly: true,
+  exec: function(msg, args) {
+    msg.channel.sendMessage(`Atlas will be rebooting in ${args} seconds`)
+    setTimeout(function() {
+      console.log("Atlas Exit")
+      process.exit()
+    }, args * 1000)
   }
 }
 
@@ -65,10 +81,11 @@ commands.whoami = {
   exec: function(msg, args) {
     var info = new discord.RichEmbed()
     info.setAuthor(`Hey ${msg.author.username}!`)
-    info.setColor("#00FBFF")
-    info.setDescription("This is what I've found about you.")
-    info.setThumbnail(msg.author.avatarURL)
-    info.addField("User ID", msg.author.id, true)
+        .setColor("#00FBFF")
+        .setDescription("This is what I've found about you.")
+        .setThumbnail(msg.author.avatarURL)
+        .addField("User ID", msg.author.id, true)
+        .addField("Created", "Your account was created on " + msg.author.createdTimestamp, true)
     msg.channel.sendEmbed(info, `${msg.author}`)
   }
 }
@@ -138,12 +155,48 @@ commands.setgame = {
   }
 }
 
+commands.m = {
+  name: "Music",
+  desc: "Changes different things for music. E.g.: play, stop, pause, etcetera.",
+  adminOnly: true,
+  exec: function(msg, args) {
+    if(args == "join") {
+      voiceChannel = atlas.channels.get(msg.member.voiceChannelID)
+      voiceChannel.join()
+      return
+    }
+    if(args == "leave" || args == "stfu" || args == "stop") {
+      voiceChannel.leave()
+      return
+    }
+    msg.reply("That's not a valid argument!")
+  }
+}
+
+commands.cena = {
+  name: "JOHN CENA",
+  desc: "Plays the John Cena mp3.",
+  exec: function(msg, args) {
+    voiceChannel = atlas.channels.get(msg.member.voiceChannelID)
+    voiceChannel.join().then(connection => {
+      const dispatcher = connection.playFile("./sounds/cena.mp3").setVolume("0.1")
+    }).catch(console.error)
+    voiceChannel.leave()
+  }
+}
+
+commands.kappa = {
+  exec: function(msg) {
+    msg.channel.sendFile("https://i.imgur.com/1l5tbGp.png")
+  }
+}
+
 atlas.on("message", msg => {
   if(msg.content.startsWith(config.prefix)) {
     var cmd = msg.content.toLowerCase().split(" ")[0].substring(1),
         args = msg.content.substring(cmd.length + 2)
     if(commands[cmd] !== undefined) {
-      if(commands[cmd].adminOnly == false) {
+      if(commands[cmd].adminOnly !== true) {
         commands[cmd].exec(msg, args)
       } else {
         if(msg.author.id === config.ownerID) {
